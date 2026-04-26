1f614f005d8e88d6833a53a321209df515417830
Nam250 <txthiv25@gmail.com>
2026-04-26 12:30:29 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(
-    body: Container(
-      decoration: BoxDecoration(gradient: LinearGradient(
-        colors: [Colors.purple, Colors.black],
-        begin: Alignment.topLeft, 
-        end:   Alignment.bottomRight
-        ),
-      ),
-    
-      
-      child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/question.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatefulWidget{
+  const Question({super.key});
+  @override
+  State<Question> createState(){
+    return _Question();
+  }
+  
+ 
+}
+
+class _Question extends State<Question>{
+  Widget build(context){
+    return Text("typeshit");
+  }
+}
